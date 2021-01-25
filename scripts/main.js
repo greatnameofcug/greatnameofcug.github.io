d397c508be96d5d8f7f9ad9ee612fe40691254e1
greatnameofcug <36263389+greatnameofcug@users.noreply.github.com>
2021-01-25 23:59:54 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/屏幕截图 2021-01-23 173201.png') {
+      myImage.setAttribute('src', 'images/屏幕截图 2021-01-23 172227.png');
+    } else {
+      myImage.setAttribute('src', 'images/屏幕截图 2021-01-23 173201.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+    }
+  }
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+myButton.onclick = function() {
+    setUserName();
+ }
